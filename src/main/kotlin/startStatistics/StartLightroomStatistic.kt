52d30c1b22990f8df4a834199db9a2c90e8064fc
edleijnse <ed@leijnse.info>
2018-05-14 21:16:57 +0200

    StartLightroomStatistics added, Jar artifact generated, see package startStatistics

--- src/main/kotlin/startStatistics/StartLightroomStatistic.kt
+++ src/main/kotlin/startStatistics/StartLightroomStatistic.kt
@@ -5,6 +5,9 @@ import java.io.File
 
 /**
  * Created by edleijnse on 13.08.17.
+ * https://www.jetbrains.com/help/idea/creating-and-running-your-first-java-application.html#package
+ * Command line:
+ * java -Dfile.encoding=UTF-8 -jar /Users/edleijnse/IdeaProjects/cloudnine/out/artifacts/kotlinsample_jar/kotlinsample.jar
  */
 fun main(args: Array<String>) {
     println("Hello, world, Ed!")
